7fbdf3abdf22b55a3860d1c0cd1aff1a26f26ea9
João Frederico de Sousa Neto <joaofredy08@gmail.com>
2020-02-05 20:18:30 -0300

    Feat(mag-faq): Finished javascript implementation

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
 
+    $('.mag-faq-title').text('Cartão de crédito e débito');
+
    $('.mag-faq .mag-icon-and-text i').on('click', function () {
        const content = $(this).data('content');
-       alert(content);
+       $('.mag-faq-title').text(content);
    });
 });
