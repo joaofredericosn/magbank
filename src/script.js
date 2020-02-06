0e5bab6e3d52e6d1ca38b1baffc3cc33996d506a
João Frederico de Sousa Neto <joaofredy08@gmail.com>
2020-02-05 21:47:56 -0300

    Feat(mag-faq): Style and active state implementation

--- src/script.js
+++ src/script.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
 
     $('.mag-faq-title').text('Cartão de crédito e débito');
+    
+    $('.mag-faq .mag-icon-and-text').first().css('color', '#fff')
 
-   $('.mag-faq .mag-icon-and-text i').on('click', function () {
-       const content = $(this).data('content');
+    $('.mag-faq .mag-icon-and-text').on('click', function () {
+
+       const content = $(this).find('i').data('content');
        $('.mag-faq-title').text(content);
+
+       $('.mag-faq .mag-icon-and-text').css('color', '#a8a8a8');
+
+       $(this).css('color', '#fff');
    });
 });
